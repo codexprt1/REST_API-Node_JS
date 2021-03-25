5460522350eb06e9838bad3d2a5015b5a0fe50ed
niket <codexprt1@gmail.com>
2021-03-25 15:04:29 +0530

    addded middlewares and post requests

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ require("dotenv/config");
 
 const port = process.env.PORT;
 
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
 app.get("/api/v1/products", (req, res) => {
+  const product = {
+    id: 0,
+    name: "Laptop",
+    os: "Linux",
+  };
   res.json({
-    msg: "Hello API",
+    product,
+  });
+});
+
+app.post("/api/v1/products", (req, res) => {
+  const newProduct = req.body;
+  console.log("NEW", newProduct);
+  res.json({
+    newProduct,
   });
 });
 
